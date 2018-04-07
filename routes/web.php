9f4598fdc4a4e4fb7d0001abc6d4d0f24f89a704
uros.purtic <uros.purtic@gmail.com>
2018-04-07 13:42:20 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('/', function () {
+    $name = 'Uros';
+    $age = 32;
+
+    return view('welcome', compact('name','age'));
+
+
+});
+
+Route::get('/cars','CarsController@index')->name('cars-list');
+Route::get('/cars/{id}','CarsController@show')->name('singe-item');
